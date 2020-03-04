0dcf461633b0c91fcfaf1a4a62ab16c485a09b90
Earl0935 <60341137+Earl0935@users.noreply.github.com>
2020-03-04 17:15:27 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+switch($_GET['p']) {
+  default:
+    include("table.php");
+    break;
+    
+  case 'add':
+    include("form_create.php");
+    break;
+    
+  case 'preview':
+    include("product-details.php");
+    break;
+}
