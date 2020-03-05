099eec64f1e79526c6ef976f2a124392df1a723d
Earl0935 <60341137+Earl0935@users.noreply.github.com>
2020-03-05 14:14:09 +0800

    Update form_create.php

--- form_create.php
+++ form_create.php
@@ -1,21 +1,25 @@
 <?php
+	$id = $_GET['id'];
+
+	$json = file_get_contents('http://rdapi.herokuapp.com/product/read_one.php?id=' . $id);
+        $data = json_decode($json, true);
 	//category
 	$json = file_get_contents('http://rdapi.herokuapp.com/category/read.php');
-	$data = json_decode($json,true);
-	$category = $data['records'];
+	$categories = json_decode($json,true);
+	$category = $categories['records'];
 ?>
 
 <form action="pro_create.php" method="POST">
 <h1> Create Product </h1>
-<input type="text" name="name" placeholder="name"/>
-<input type="text" name="description" placeholder="description"/>
-<input type="text" name="price" placeholder="price"/>
+<input type="text" name="name" value="<?php if($id) echo $data['name']" placeholder="name"/>
+<input type="text" name="description" value="<?php if($id) echo $data['description']" placeholder="description"/>
+<input type="text" name="price" value="<?php if($id) echo $data['price']" placeholder="price"/>
 <select name="category">
 <option value="">--Category--</option>
 	<?php
       foreach($category as $cview){
     ?>
-		<option value="<?php echo $cview['id']?>"><?php echo $cview['name']?></option>
+		<option value="<?php echo $cview['id']?>" <?php if ($category['id'] == $data['category_id']) echo "selected" ?>><?php echo $cview['name']?></option>
     <?php
       }
     ?>
